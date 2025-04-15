ab0fab5ae9ba8ad943ea80b4a939929cd215cf2b
Akylbek <a.mendibaev00@mail.ru>
2025-04-15 14:19:16 +0500

    Deleting other files

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /project/solana
-source $HOME/.cargo/env
-cargo build-bpf 
--- scripts/create_wallet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /project
-source $HOME/.cargo/env
-solana-keygen new --no-bip39-passphrase -o wallet.json
-solana config set --url devnet
-solana airdrop 1 
